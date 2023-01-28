f39121974033786755f24829c2092af319d6ecf3
Ryan Bertram <ryan.bertram@stackfields.com>
2023-01-28 12:06:00 -0500

    OMG this was a discovery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
   include JwtToken
 
-  before_action :authenicate_user
+  before_action :authenticate_user
 
   private
-    def authenicate_user
+    def authenticate_user
       header = request.headers['Authorization']
       header = header.split(' ').last if header
       begin
-        @decoded = JwtToken.decode(header)
+        @decoded = jwt_decode(header)
         @current_user = User.find(@decoded[:user_id])
       rescue ActiveRecord::RecordNotFound => e
         render json: { errors: e.message }, status: :unauthorized
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,11 +1,12 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :authenicate_user
+  skip_before_action :authenticate_user
   # post /auth/login
   def login
     @user = User.find_by_email(params[:email])
-    if @user&.authenicate(params[:password])
-      token = Jwtoken.encode(user_id: @user.id)
+    if @user&.authenticate(params[:password])
+      token = jwt_encode({:user_id => @user.id})
       time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.user_name}, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
